2c46f269ec978b79b03a9a3bc152ba98a1f02459
Anna Malanova <anna.malanova@slalom.com>
2019-02-09 00:38:44 -0600

    updated

---
+++ first_groovy.groovy
@@ -0,0 +1,13 @@
+class GroovyTut{
+  static void main(String[] args){
+    println("Hello World")
+    def age = "Dog";
+    println(age)
+
+    // Regular numbers
+    println('5 + 4 = ' + (5 + 4));
+    println('5 - 4 = ' + (5 - 4));
+    println('5 / 2 = ' + (5.intdiv(2)));
+
+  }
+}
